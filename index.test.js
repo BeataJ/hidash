e945e72087a5bdf66060bb3501028ab02fcc7cf3
beata <beata@emc2wave.com>
2020-04-15 16:36:37 -0700

    add map to test fun

--- index.test.js
+++ index.test.js
@@ -16,16 +16,18 @@ test('The forEach function', () => {
   }
 });
 
-const result = map([1, 2, 3], (value) => {
-  return value * 2;
-});
+test('The map function', () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
 
-if (result[0] !== 2) {
-  throw new Error(`Expected to find 2, but found ${result[0]}`);
-}
-if (result[1] !== 4) {
-  throw new Error(`Expected to find 4, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-  throw new Error(`Expected to find 2, but found ${result[2]}`);
-}
+  if (result[0] !== 2) {
+    throw new Error(`Expected to find 2, but found ${result[0]}`);
+  }
+  if (result[1] !== 4) {
+    throw new Error(`Expected to find 4, but found ${result[1]}`);
+  }
+  if (result[2] !== 6) {
+    throw new Error(`Expected to find 2, but found ${result[2]}`);
+  }
+});
