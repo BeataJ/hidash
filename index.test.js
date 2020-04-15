579db0eb266c92781f14e8effe00549035d752f3
beata <beata@emc2wave.com>
2020-04-15 16:34:55 -0700

    add forEach to test fn

--- index.test.js
+++ index.test.js
@@ -5,14 +5,16 @@ const test = (desc, fn) => {
   fn();
 };
 
-let sum = 0;
-forEach([1, 2, 3], (value) => {
-  sum += value;
-});
+test('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
 
-if (sum !== 6) {
-  throw new Error('Expected summing array to equal 6');
-}
+  if (sum !== 6) {
+    throw new Error('Expected summing array to equal 6');
+  }
+});
 
 const result = map([1, 2, 3], (value) => {
   return value * 2;
