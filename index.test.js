5df02360d5368e53062f51d2f5716f1575d13f23
beata <beata@emc2wave.com>
2020-04-16 10:11:40 -0700

    add assert.strictEqual to test map

--- index.test.js
+++ index.test.js
@@ -24,13 +24,7 @@ test('The map function', () => {
     return value * 2;
   });
 
-  if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`);
-  }
-  if (result[1] !== 4) {
-    throw new Error(`Expected to find 4, but found ${result[1]}`);
-  }
-  if (result[2] !== 6) {
-    throw new Error(`Expected to find 2, but found ${result[2]}`);
-  }
+  assert.strictEqual(result[0], 2);
+  assert.strictEqual(result[1], 4);
+  assert.strictEqual(result[2], 6);
 });
