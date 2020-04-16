f7067d9e3f1ce280a068b65fdecafd09cebfbd91
beata <beata@emc2wave.com>
2020-04-16 10:14:19 -0700

    add assert.deepStrictEqual

--- index.test.js
+++ index.test.js
@@ -24,7 +24,9 @@ test('The map function', () => {
     return value * 2;
   });
 
-  assert.strictEqual(result[0], 2);
-  assert.strictEqual(result[1], 4);
-  assert.strictEqual(result[2], 6);
+  assert.deepStrictEqual(result, [2, 4, 6]);
+
+  // assert.strictEqual(result[0], 2);
+  // assert.strictEqual(result[1], 4);
+  // assert.strictEqual(result[2], 6);
 });
